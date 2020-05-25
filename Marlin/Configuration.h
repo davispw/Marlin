624055a65185eac5f971e84b87af6d6eaa97ec78
Peter Davis <davispw@gmail.com>
2020-05-25 11:12:23 -0700

    Set dimensions to 300x300x350

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1090,16 +1090,16 @@
 //@
 
 // The size of the print bed
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 210
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -5
+#define X_MIN_POS -1
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS 215
-#define Z_MAX_POS 220
+#define Y_MAX_POS 300
+#define Z_MAX_POS 350
 
 /**
  * Software Endstops
