b494c81944fc52373efaea95ccc5bd55e7f51e0e
Peter Davis <davispw@gmail.com>
2020-05-25 10:58:39 -0700

    250k baud rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -129,7 +129,7 @@
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
 // @fixing octoprint connection issues - I guess RPi may have issues with higher baud rates.
-#define BAUDRATE 115200
+#define BAUDRATE 250000
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
